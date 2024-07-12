ca327b934c0e46a1ba5e4b642e3e6dcddd55aadf
tjwns1400 <tjwns1400@gmail.com>
2024-07-12 13:05:29 +0900

    엑셀로 출력

--- blogcrawl/blog.py
+++ blogcrawl/blog.py
@@ -2,8 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
-from selenium.webdriver.chrome.service import Service as ChromeService #크롬으로 열기
-from selenium.webdriver import ChromeOptions #크롬 옵션
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver import ChromeOptions
+import pandas as pd
 
 # keyword = input("검색할 키워드를 입력하세요: ")
 keyword = "웨딩플래너"
@@ -18,6 +19,7 @@ def driversetup(url):
     driver.get(url)
     time.sleep(3)
     return driver
+
 # 네이버 검색 페이지 접근
 naver_url = 'https://www.naver.com'
 
@@ -27,7 +29,6 @@ driver = driversetup(naver_url)
 search_box = driver.find_element(By.NAME, 'query')
 
 # 키워드 입력 및 검색
-
 search_box.send_keys(keyword)
 search_box.send_keys(Keys.RETURN)
 
@@ -36,18 +37,17 @@ time.sleep(2)  # 페이지 로딩 대기
 blog_tab = driver.find_element(By.CSS_SELECTOR, 'a[href*="?ssc=tab.blog.all"]')
 blog_tab.click()
 
-
 # 최신순 정렬 옵션 선택
 time.sleep(2)  # 페이지 로딩 대기
 latest_option = driver.find_element(By.XPATH, '//a[@role="option" and contains(text(), "최신순")]')
 latest_option.click()
-
 
 # 블로그 리스트 크롤링
 time.sleep(2)  # 페이지 로딩 대기
 blog_posts = driver.find_elements(By.CLASS_NAME, 'bx')
 time.sleep(2) 
 blog_links = []
+
 for post in blog_posts:
     try:
         if 'user_info' in post.get_attribute('innerHTML'):
@@ -56,9 +56,27 @@ for post in blog_posts:
             blog_links.append(link)
     except:
         print("user_info 또는 링크를 찾을 수 없습니다.")
+
+# Targetmail 및 Targetlink 생성
+data = []
+for link in blog_links:
+    blog_id = link.split('/')[-1]
+    target_mail = f"{blog_id}@naver.com"
+    data.append({"Targetmail": target_mail, "Targetlink": link})
+
+# DataFrame 생성
+df = pd.DataFrame(data)
+
+# 현재 시간 가져오기
+current_time = time.strftime("%Y%m%d_%H%M%S")
+
+# 엑셀 파일로 출력
+output_filename = f"output_{current_time}.xlsx"
+df.to_excel(output_filename, index=False)
+
 # 결과 출력
-for i, blog in enumerate(blog_links):
-    print(f"블로그 {i+1} 내용:\n{blog}\n")
+for i, row in df.iterrows():
+    print(f"Targetmail: {row['Targetmail']}, Targetlink: {row['Targetlink']}")
 
 # 드라이버 종료
 driver.quit()
